fd9bc7a1413649ba4c9e0bb468e5c1d63b7a3c28
Ismael Ramos Silvan <ramossilvanismael@gmail.com>
2017-01-27 13:50:48 +0100

    feat(sqlite): added database example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {Platform} from 'ionic-angular';
-import {StatusBar, Splashscreen} from 'ionic-native';
+import {StatusBar, Splashscreen, SQLite} from 'ionic-native';
 
 import {TabsPage} from '../pages/tabs/tabs';
 
@@ -13,10 +13,21 @@ export class MyApp {
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+      let db = new SQLite();
+      db.openDatabase({
+        name: "data.db",
+        location: "default"
+      }).then(() => {
+        db.executeSql("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT)", {}).then((data) => {
+          console.log("TABLE CREATED: ", data);
+        }, (error) => {
+          console.error("Unable to execute sql", error);
+        })
+      }, (error) => {
+        console.error("Unable to open database", error);
+      });
     });
   }
 }
