8e015252295e65b6b1f7d0a4aaf75daea737452e
Ismael Ramos Silvan <ramossilvanismael@gmail.com>
2017-02-09 01:11:38 +0100

    feat(translations): added texts for items and categories

--- src/app/items/items-list/items-list.service.ts
+++ src/app/items/items-list/items-list.service.ts
@@ -38,14 +38,138 @@ export class ItemsService {
   loadCategories() {
     this.categoriesDB.post({id: 'documentation'});
     this.categoriesDB.post({id: 'clothes'});
+    this.categoriesDB.post({id: 'dressing_case'});
+    this.categoriesDB.post({id: 'media'});
+    this.categoriesDB.post({id: 'mountain'});
+    this.categoriesDB.post({id: 'beach'});
+    this.categoriesDB.post({id: 'work'});
+    this.categoriesDB.post({id: 'kit'});
+    this.categoriesDB.post({id: 'other'});
   }
 
   loadItems() {
-    this.itemsDB.post({id: 'credit-cards', categoryId: 'documentation'});
-    this.itemsDB.post({id: 'driver-license', categoryId: 'documentation'});
+    this.itemsDB.post({id: "passport", categoryId: 'documentation'});
+    this.itemsDB.post({id: "driver_license", categoryId: 'documentation'});
+    this.itemsDB.post({id: "credit_cards", categoryId: 'documentation'});
+    this.itemsDB.post({id: "tickets", categoryId: 'documentation'});
 
-    this.itemsDB.post({id: 'footwear', categoryId: 'clothes'});
-    this.itemsDB.post({id: 'glasses', categoryId: 'clothes'});
+    this.itemsDB.post({id: "skirts", categoryId: 'clothes'});
+    this.itemsDB.post({id: "jewelry", categoryId: 'clothes'});
+    this.itemsDB.post({id: "wristwatch", categoryId: 'clothes'});
+    this.itemsDB.post({id: "glasses", categoryId: 'clothes'});
+    this.itemsDB.post({id: "footwear", categoryId: 'clothes'});
+    this.itemsDB.post({id: "socks", categoryId: 'clothes'});
+    this.itemsDB.post({id: "shorts", categoryId: 'clothes'});
+    this.itemsDB.post({id: "trousers", categoryId: 'clothes'});
+    this.itemsDB.post({id: "underwear", categoryId: 'clothes'});
+    this.itemsDB.post({id: "t_shirts", categoryId: 'clothes'});
+    this.itemsDB.post({id: "cap", categoryId: 'clothes'});
+    this.itemsDB.post({id: "hat", categoryId: 'clothes'});
+    this.itemsDB.post({id: "rain_coat", categoryId: 'clothes'});
+    this.itemsDB.post({id: "scarf", categoryId: 'clothes'});
+    this.itemsDB.post({id: "gloves", categoryId: 'clothes'});
+    this.itemsDB.post({id: "night_suit", categoryId: 'clothes'});
+    this.itemsDB.post({id: "slippers", categoryId: 'clothes'});
+    this.itemsDB.post({id: "scarves", categoryId: 'clothes'});
+    this.itemsDB.post({id: "makeup", categoryId: 'clothes'});
+    this.itemsDB.post({id: "lipstick", categoryId: 'clothes'});
+
+    this.itemsDB.post({id: "tampons", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "salvaslip", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "cleansing_wipes", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "moisturizer", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "foam_hair", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "conditioner", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "gums", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "nail_polish", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "nail_file", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "perfume", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "bath_gel", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "shampoo", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "sponge", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "dryer", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "comb", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "deodorant", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "toothbrush", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "toothpaste", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "colony", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "gomina", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "creams", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "ear_buds", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "shaving_gel", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "safety_razor", categoryId: 'dressing_case'});
+    this.itemsDB.post({id: "nail_clippers", categoryId: 'dressing_case'});
+
+    this.itemsDB.post({id: "map", categoryId: 'mountain'});
+    this.itemsDB.post({id: "compass", categoryId: 'mountain'});
+    this.itemsDB.post({id: "waist", categoryId: 'mountain'});
+    this.itemsDB.post({id: "lantern", categoryId: 'mountain'});
+    this.itemsDB.post({id: "binoculars", categoryId: 'mountain'});
+    this.itemsDB.post({id: "rope", categoryId: 'mountain'});
+    this.itemsDB.post({id: "whistle", categoryId: 'mountain'});
+    this.itemsDB.post({id: "food_tools", categoryId: 'mountain'});
+    this.itemsDB.post({id: "pencil_and_paper", categoryId: 'mountain'});
+    this.itemsDB.post({id: "sleeping_bag", categoryId: 'mountain'});
+    this.itemsDB.post({id: "swiss_knife", categoryId: 'mountain'});
+    this.itemsDB.post({id: "lighter", categoryId: 'mountain'});
+    this.itemsDB.post({id: "thermos", categoryId: 'mountain'});
+    this.itemsDB.post({id: "pillow", categoryId: 'mountain'});
+
+    this.itemsDB.post({id: "swimsuit", categoryId: 'beach'});
+    this.itemsDB.post({id: "sunscreen", categoryId: 'beach'});
+    this.itemsDB.post({id: "after_sun", categoryId: 'beach'});
+    this.itemsDB.post({id: "thongs", categoryId: 'beach'});
+    this.itemsDB.post({id: "towel", categoryId: 'beach'});
+    this.itemsDB.post({id: "parasol", categoryId: 'beach'});
+    this.itemsDB.post({id: "mats", categoryId: 'beach'});
+    this.itemsDB.post({id: "racquets", categoryId: 'beach'});
+    this.itemsDB.post({id: "cards", categoryId: 'beach'});
+
+    this.itemsDB.post({id: "camera", categoryId: 'media'});
+    this.itemsDB.post({id: "charger", categoryId: 'media'});
+    this.itemsDB.post({id: "mobile", categoryId: 'media'});
+    this.itemsDB.post({id: "ipod", categoryId: 'media'});
+    this.itemsDB.post({id: "headphones", categoryId: 'media'});
+    this.itemsDB.post({id: "radio", categoryId: 'media'});
+    this.itemsDB.post({id: "GPS", categoryId: 'media'});
+    this.itemsDB.post({id: "tablet", categoryId: 'media'});
+    this.itemsDB.post({id: "memory_card", categoryId: 'media'});
+
+    this.itemsDB.post({id: "ties", categoryId: 'work'});
+    this.itemsDB.post({id: "breath_mints", categoryId: 'work'});
+    this.itemsDB.post({id: "pants", categoryId: 'work'});
+    this.itemsDB.post({id: "skirt_suit", categoryId: 'work'});
+    this.itemsDB.post({id: "jacket", categoryId: 'work'});
+    this.itemsDB.post({id: "shirts", categoryId: 'work'});
+    this.itemsDB.post({id: "cufflink", categoryId: 'work'});
+    this.itemsDB.post({id: "shoes", categoryId: 'work'});
+    this.itemsDB.post({id: "phone", categoryId: 'work'});
+    this.itemsDB.post({id: "laptop", categoryId: 'work'});
+    this.itemsDB.post({id: "flash_drive", categoryId: 'work'});
+    this.itemsDB.post({id: "work_material", categoryId: 'work'});
+
+    this.itemsDB.post({id: "band_aids", categoryId: 'kit'});
+    this.itemsDB.post({id: "scissors", categoryId: 'kit'});
+    this.itemsDB.post({id: "paracetamol", categoryId: 'kit'});
+    this.itemsDB.post({id: "sticking_plaster", categoryId: 'kit'});
+    this.itemsDB.post({id: "thermometer", categoryId: 'kit'});
+    this.itemsDB.post({id: "antiseptic", categoryId: 'kit'});
+
+    this.itemsDB.post({id: "keys", categoryId: 'other'});
+    this.itemsDB.post({id: "cash", categoryId: 'other'});
+    this.itemsDB.post({id: "umbrella", categoryId: 'other'});
+    this.itemsDB.post({id: "voltage_converter", categoryId: 'other'});
+    this.itemsDB.post({id: "anti_mosquito", categoryId: 'other'});
+    this.itemsDB.post({id: "travel_locks", categoryId: 'other'});
+    this.itemsDB.post({id: "plastic_bags", categoryId: 'other'});
+    this.itemsDB.post({id: "condoms", categoryId: 'other'});
+    this.itemsDB.post({id: "toys", categoryId: 'other'});
+    this.itemsDB.post({id: "baby_food", categoryId: 'other'});
+    this.itemsDB.post({id: "book", categoryId: 'other'});
+    this.itemsDB.post({id: "snacks", categoryId: 'other'});
+    this.itemsDB.post({id: "shoes_insoles", categoryId: 'other'});
+    this.itemsDB.post({id: "water", categoryId: 'other'});
+    this.itemsDB.post({id: "travel_pillow", categoryId: 'other'});
   }
 
   addItem(item) {
