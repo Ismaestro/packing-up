e99973ba1287d2b670ad4153bbdff352fe44c27c
Ismael Ramos Silvan <ramossilvanismael@gmail.com>
2017-02-18 14:20:24 +0100

    feat(loading): removed splash screen and added a loading page

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,4 +4,7 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app.module';
 
 enableProdMode();
-platformBrowserDynamic().bootstrapModule(AppModule);
+
+platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
+  document.getElementById('loading').style.display = 'none';
+});
